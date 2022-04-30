ef183aae5e148e66b7447f65a21e76d5129504be
Agnes <koinageagness@gmail.com>
2022-04-30 19:48:39 +0300

    call app.config['name_of_object'] in request.py

--- app/request.py
+++ app/request.py
@@ -11,22 +11,3 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
-
-def get_news(category):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_news_url = base_url.format(category,api_key)
-
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
-
-        news_results = None
-
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
-
-
-    return news_results
