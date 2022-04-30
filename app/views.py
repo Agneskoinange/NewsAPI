140ccc82d2ec30bf840d15622e86554ea753eff0
Agnes <koinageagness@gmail.com>
2022-04-30 18:26:57 +0300

    add add HTML structure and declare if statement

--- app/views.py
+++ app/views.py
@@ -8,8 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'News Application'
-    return render_template('index.html',message = message)
+    # message = 'News Application'
+    # return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<int:news_id>')
 def movie(news_id):
