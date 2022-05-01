42ef55747a75f993801d88d51cc6b6dda3ec9155
Agnes <koinageagness@gmail.com>
2022-05-01 09:02:24 +0300

    display articles

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_news, get_news, search_news
+from models import review
+from .forms import ReviewForm
+Review = review.articles
 
 
 # Views
@@ -45,3 +48,18 @@ def search(news_name):
     searched_news = search_news(news_name_format)
     title = f'search results for {news_name}'
     return render_template('search.html',news = searched_news)
+
+@app.route('/news/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    news = get_news(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(news.id,title,news.title,review)
+        new_review.save_review()
+        return redirect(url_for('news',id = news.id ))
+
+    title = f'{news.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=news)
--- models/articles.py
+++ models/articles.py
@@ -2,10 +2,22 @@ class Articles:
   '''
   Articles class to define Article objects
   '''
-
+  all_articles = []
   def __init__(self, author, title, imageurl, publishedAt, url):
     self.author = author
     self.title = title
     self.imageurl =imageurl
     self.publishedAt = publishedAt
     self.url = url
+
+
+    @classmethod
+    def get_articles(cls,id):
+
+        response = []
+
+        for review in cls.all_articles:
+            if review.news_id == id:
+                response.append(Articles)
+
+        return response
