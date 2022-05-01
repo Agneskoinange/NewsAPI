cd3a0087fda437cfccd2f5e069abe67fe4a3d87e
Agnes <koinageagness@gmail.com>
2022-05-01 10:46:33 +0300

    move all the details from previous structure to the new structure

--- models/articles.py
+++ models/articles.py
@@ -1,18 +1,5 @@
 class Articles:
-  '''
-  Articles class to define Article objects
-  '''
-  all_articles = []
-  def __init__(self, author, title, imageurl, publishedAt, url):
-    self.author = author
-    self.title = title
-    self.imageurl =imageurl
-    self.publishedAt = publishedAt
-    self.url = url
 
-
-    @classmethod
-    def get_articles(cls,id):
 
         response = []
 
