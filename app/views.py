ee748cc9b44b5ab7638b9fdf43854407c9164790
Agnes <koinageagness@gmail.com>
2022-05-01 08:32:59 +0300

    create serach view function on viws.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_news, get_news
+from .request import get_news, get_news, search_news
 
 
 # Views
@@ -28,3 +28,14 @@ def news(id):
     title = f'{news.title}'
 
     return render_template('news.html',title = title,news = news)
+
+@app.route('/search/<news_name>')
+def search(news_name):
+    '''
+    View function to display the search results
+    '''
+    news_name_list = news_name.split(" ")
+    news_name_format = "+".join(news_name_list)
+    searched_news = search_news(news_name_format)
+    title = f'search results for {news_name}'
+    return render_template('search.html',movies = searched_news)2
