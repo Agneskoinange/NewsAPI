645c512f8326056c81e0d727f2f1f5d4043ca195
Agnes <koinageagness@gmail.com>
2022-05-01 07:27:50 +0300

    add more category requests in views.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config:
     '''
     General configuration parent class
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from app import app
 from .request import get_news
 
+
 # Views
 @app.route('/')
 def index():
@@ -12,9 +13,10 @@ def index():
   
     # Getting popular movie
     popular_news = get_news('popular')
-    print(popular_news)
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_playing')
     title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news)
 
 @app.route('/news/<int:news_id>')
 def movie(news_id):
